0e90ea7257b9133716f2f62bdf50d418c735e5f6
LEE YEJIN <dldulwls@naver.com>
2024-03-21 14:52:18 +0900

    💄메인페이지 ui 작업

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,20 @@
 import Header from '../../components/Header';
-
+import ItemCard from '../../components/ItemCard/ItemCard';
+import { ItemCardList } from './styles';
 // import { itemList, filteredList, searchItem } from '../../service/itemService';
+
 function Home() {
   return (
     <div>
       <Header />
+      <img src="/assets/banner.png" alt="banner" />
+      <ItemCardList>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </ItemCardList>
     </div>
   );
 }
